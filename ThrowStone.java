34c11861e89b6ae6183c24812b3bc0a9f66c4b99
“Nadim” <nadimmansu@gmail.com>
2021-09-12 21:52:46 -0700

    Add New File: Thread example using Runnable interface

---
+++ ThrowStone.java
@@ -0,0 +1,42 @@
+/**Author : Nadim Mansuri.
+*  Date :10/09/2021. 
+*  Creating a ThrowStone class using  Runnable interface .
+*/
+ public class ThrowStone implements Runnable {
+	
+ public String weight = null;
+ public ThrowStone(String stoneweight) {
+ 	this.weight = stoneweight;
+   }
+
+ /**
+* methods use to run stone weight.
+*/
+	public void run() {
+		for(int i = 0; i < 50; i++) {
+				System.out.println(" Stone " + weight);
+		}	
+	}
+}
+/**Author : Nadim Mansuri.
+*  Date :10/09/2021. 
+*  Creating a RunThroxStone class using run ThrowStone class.
+*/
+
+class RunThrowStone {
+	public static void main(String[] args) {
+		Thread thread;
+
+		ThrowStone throwStone = new ThrowStone("10kg");
+		thread = new Thread(throwStone);
+		thread.start();
+	
+		ThrowStone throwStone1 = new ThrowStone("5kg");
+		thread = new Thread(throwStone1);
+		thread.start();	
+		ThrowStone throwStone2 = new ThrowStone("15kg");
+		thread = new Thread(throwStone2);
+		thread.start();
+		
+	}
+}
