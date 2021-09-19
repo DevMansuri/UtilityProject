355b98c2b8176509599fb8cde2447c7e8dabd676
“Nadim” <nadimmansu@gmail.com>
2021-09-19 18:03:09 +0530

    Add New File : Polymorphism Example

---
+++ PrincipalOffice.java
@@ -0,0 +1,42 @@
+/**
+*@Author : Nadim mansuri
+*Date : 19/09/2021
+*Basic polymorphism Example.
+**/
+class PrincipalOffice {
+	private float accaountBalance;
+    public String collageName = "mist";
+    private float facultySalary;
+    public int studentFees;
+
+    public float getBalance(){
+    	return accaountBalance;
+    }
+	public void copyCheck() {
+
+		}
+}
+
+class Class1st extends PrincipalOffice {
+	public void copyCheck() {
+		System .out.println("yes Class1st copy is check");
+	}
+}
+
+class Class2nd extends PrincipalOffice {
+	public void copyCheck() {
+		System.out.println("yes Class2nd copyCheck");
+	}
+
+	public static void main(String[] args) {
+		PrincipalOffice principalOffice;
+		principalOffice = new Class1st();
+		principalOffice.copyCheck();
+
+		principalOffice = new Class2nd();
+		principalOffice.copyCheck();
+
+		System.out.println("collage Name is :" + principalOffice.collageName);
+		System.out.println("Account Balance is :" + principalOffice.getBalance());	
+	}
+} 
