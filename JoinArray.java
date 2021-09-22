088cc988f2f99817c5d32bea662a85b2bc15e527
“Nadim” <nadimmansu@gmail.com>
2021-09-22 16:14:28 +0530

    Add New File : Serializable and I/o example

---
+++ JoinArray.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class JoinArray implements Serializable  {
+
+	public String fullString = "TEST";
+
+	/**
+	*	Source:
+	*/
+	public void printArray() {
+	 	String[] greeting = {"How", "are", "you", "nadim"};
+	    String delimiter = " ";
+	    fullString = String.join(delimiter, greeting);
+	   	System.out.println(fullString);	
+    }
+
+	public static void main(String[] args) {
+		JoinArray joinArray = new JoinArray();
+		joinArray.printArray();
+		
+		try {
+			FileOutputStream fileOut = new FileOutputStream("C:/Users/123/Documents/training/test4.log");
+	    	ObjectOutputStream out = new ObjectOutputStream(fileOut);
+     
+	        out.writeObject(joinArray);
+	        System.out.printf("detail is saved");
+	        out.close();
+		    fileOut.close();
+      	} catch (Exception i) {
+           System.out.println("print array error");     	
+      	} finally {
+
+      	}
+	}
+}
