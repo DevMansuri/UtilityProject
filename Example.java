ae4864ddafb910b9b30e5d77225b5d64725d1cbc
“Nadim” <nadimmansu@gmail.com>
2021-09-24 18:10:20 +0530

    Update File : Add new method

--- Example.java
+++ Example.java
@@ -1,14 +1,28 @@
 /**
 *@Author : Nadim mansuri
 *Date : 24/09/2021
-*purpose : how to print table of two.
+*purpose : how to print tables.
 **/
 class Example {
-	public static void main(String[] args) {
-		for (int a = 1 ; a < 20; a++) 
-		{
-			a = a + 1;
-			System.out.println(a);
-		}
+ /**
+ method purpose : how to print table of two.
+ **/
+ public void tableOfTwo() {
+ 	 for (int a = 1 ; a < 20; a++) {
+		 a = a + 1;
+		 System.out.println(a);
+	    }
+	    System.out.println("------------------------------------------------------"); 
+    }   
+}
+/**
+*@Author : Nadim mansuri
+*Date : 24/09/2021
+*purpose : purpose of this class to run Example class.
+**/
+class RunTable {    
+    public static void main(String[] args) {
+	 Example example = new Example();
+	 example.tableOfTwo();
 	}
 }
