d43633bca3258174bb6a0f90ac3817b09f54a44a
“Nadim” <nadimmansu@gmail.com>
2021-09-24 16:47:30 +0530

    Add New File : how to print tables

---
+++ Example.java
@@ -0,0 +1,14 @@
+/**
+*@Author : Nadim mansuri
+*Date : 24/09/2021
+*purpose : how to print table of two.
+**/
+class Example {
+	public static void main(String[] args) {
+		for (int a = 1 ; a < 20; a++) 
+		{
+			a = a + 1;
+			System.out.println(a);
+		}
+	}
+}
