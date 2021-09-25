755457911b5bbb6661ddd4eb655e1ad6408b4b33
“Nadim” <nadimmansu@gmail.com>
2021-09-25 11:15:43 +0530

    Update File :add new method

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,14 +5,32 @@
 *           fibonacci series.
 **/
 class Fibonacci {
-    int n1= 0;
-	int n2= 1;
-	int n3 ;
-	/**
+    /**
 	*method purpose : how to print first ten number of fibonacci series.
 	**/
 	public void firstTenElement() {
+		int n1= 0;
+		int n2= 1;
+		int n3 ;
+	
 		for (int num = 0; num < 10; num ++) {
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+			System.out.println(n3);
+        }
+
+		System.out.println("-----------------------------------------------------------");
+	}
+	/**
+	*method purpose : how to print first twenty number of fibonacci series.
+	**/
+	public void firstTwentyElement() {
+		int n1 = 0;
+		int n2 = 1;
+		int n3 ;
+
+		for (int num = 0; num < 20; num ++) {
 			n3 = n1 + n2;
 			n1 = n2;
 			n2 = n3;
@@ -24,5 +42,6 @@ class RunFibonacci {
 	public static void main(String[] args) {
 		Fibonacci fibonacci = new Fibonacci();
 		fibonacci.firstTenElement();
+		fibonacci.firstTwentyElement();
 	}
 }
