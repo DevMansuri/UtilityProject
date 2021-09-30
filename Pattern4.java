0a4eb91194e92b9d21855012aaafe05d5966ed9e
“Nadim” <nadimmansu@gmail.com>
2021-09-30 15:26:17 +0530

    Add New File : Pattern Example

---
+++ Pattern4.java
@@ -0,0 +1,22 @@
+/**
+*@Author : Nadim mansuri.
+*Date : 30/09/2021
+*purpose : how to print pattern      *
+	                                * *
+	                               * * *
+	                              * * * *
+	                             * * * * *   
+**/
+class Pattern4 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int  j = 5 ; j>=i ; j-- ) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i ; k++ ) {
+				System.out.print(" *");
+			}
+		 System.out.println();
+		}
+	}
+}
