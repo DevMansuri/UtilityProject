1d62326f01181644a5668efd91726bfa0f1be6ab
“Nadim” <nadimmansu@gmail.com>
2021-10-04 16:39:41 +0530

    Add New File : Pattern Example -11

---
+++ Pattern11.java
@@ -0,0 +1,36 @@
+/**
+*@Author : Nadim mansuri.
+*Date : 4/10/2021
+*purpose : how to prinnt pattern -:    * 
+	                                  * *
+	                                 * * *
+	                                * * * *
+	                               * * * * *
+	                               * * * * *
+	                                * * * *
+	                                 * * *
+	                                  * *
+	                                   *
+	**/
+class Pattern11 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int  j = 5 ; j >= i ; j-- ) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i ; k++ ) {
+				System.out.print(" *");
+			}
+		 System.out.println();
+		}
+		for(int i = 1 ; i <= 5; i ++) {
+			for ( int j = 1; j<=i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 5 ; k >= i; k-- ) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+}
