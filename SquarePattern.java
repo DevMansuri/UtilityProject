eded7fd0208661daa45ac34612b6baa6d8b246d1
“Nadim” <nadimmansu@gmail.com>
2021-10-22 17:52:11 +0530

    Add New File : SquarePattern Example

---
+++ SquarePattern.java
@@ -0,0 +1,14 @@
+/**
+Author : Nadim mansuri.
+Date : 5/10/2021.
+*/
+class SquarePattern {
+	public static void main(String[] args) {
+		for (int i = 1; i <=5; i++ ) {
+			for(int j = 1; j <=5; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
