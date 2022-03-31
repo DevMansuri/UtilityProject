c13eb3bcc35f4b8da0b3b92e09709c70d4aa1ce6
“Nadim” <nadimmansu@gmail.com>
2022-03-31 11:00:54 +0530

    Add new File :

---
+++ SecondLargestSecondSmalest.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class SecondLargestSecondSmallest {
+    public static void secondLargestNumber( ) {
+        int n, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array(Minimum 2):");
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+                
+        System.out.println("Second Largest:"+a[n-2]);
+        
+
+    }
+    public static void main(String[] args) {
+        secondLargestNumber();
+    }
+}
